6bedaa907ed3ddd8825b5c73b11238032d00a6c0
Davide Di Mauro <davidedimauro88@gmail.com>
2017-05-17 15:43:13 +0100

    add gif for demo

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php
+<h1><?php
     echo "It Works";
     error_log("Log works too");
-?>
+?></h1>
+
+<iframe src="https://giphy.com/embed/3oKIPsx2VAYAgEHC12" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/DNCE-3oKIPsx2VAYAgEHC12">via GIPHY</a></p>
